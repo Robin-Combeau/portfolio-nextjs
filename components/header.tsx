8da8b9b950707bc37d8b2ff28fb34dd03bfb5f28
Robin C. <robin.combeau@gmail.com>
2024-01-09 23:31:10 +0100

    feat header and params

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="">
+        <div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-black border-opacity"></div>
+    </header>
+  )
+}
---
+++ lib/data.ts
@@ -0,0 +1,47 @@
+import React from "react";
+
+export const links = [
+  {
+    name: "Accueil",
+    hash: "#home",
+  },
+  {
+    name: "À propos",
+    hash: "#about",
+  },
+  {
+    name: "Compétences",
+    hash: "#skills",
+  },
+  {
+    name: "Projets",
+    hash: "#projects",
+  },
+  {
+    name: "Expériences",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const skills = [
+  "PHP",
+  "Laravel",
+  "Javascript",
+  "React.js",
+  "Next.js",
+  "jQuery",
+  "CSS",
+  "Tailwind",
+  "HTML",
+  "Zend",
+  "SQL",
+  "Git",
+  "CI/CD",
+  "UI Design",
+  "Figma",
+  "Framer Motion",
+] as const;
